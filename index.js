88fdcc606fd0d89bfacae4f33d5d1d5d9b3ee1ff
Akash01997 <akashshinde558@gmail.com>
2024-05-29 13:41:10 +0530

    Post Method

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express")
+const {createTodo} = require("./types");
+
 const app = express()
 
 app.use(express.json())
 
-app.get("/", function(){
-    
+
+
+
+app.post("/todos", function(req, res){
+    const createPayload = req.body;
+    const parsePayLoad = createTodo.safeParse(createPayload);
+    if(!parsePayLoad.success){
+        req.status(411).json({
+            msg : "You sent the wrong input"
+        })
+        return
+    }
+})
+
+app.get("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req, res){
+
 })
 
 app.listen(8080)
