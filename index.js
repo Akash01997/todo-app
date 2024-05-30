9d510264ec15bacf73677dc1f4e5919f2f9bdaad
Akash01997 <akashshinde558@gmail.com>
2024-05-30 10:29:36 +0530

    POST and PUT

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const app = express()
 
 app.use(express.json())
 
-
-
-
 app.post("/todos", function(req, res){
     const createPayload = req.body;
     const parsePayLoad = createTodo.safeParse(createPayload);
     if(!parsePayLoad.success){
         req.status(411).json({
-            msg : "You sent the wrong input"
+            msg : "You sent the wrong input",
         })
         return
     }
@@ -24,7 +21,14 @@ app.get("/todos", function(req, res){
 })
 
 app.put("/completed", function(req, res){
-
+    const updateTodo = req.body;
+    const parsePayLoad = updateTodo.safeParse(updateTodo);
+    if(!parsePayLoad.success){
+        res.status(411).json({
+            msg :"You sent a wrong inputs",
+        })
+        return;
+    }
 })
 
 app.listen(8080)
